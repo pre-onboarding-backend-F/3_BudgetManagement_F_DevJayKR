12b6a9a335c25979e13d12180c9bcf1bd71d6450
Junsung Choi <choiexe1@gmail.com>
2023-11-14 18:51:41 +0900

    refactor: app provider
    - 기존 bootstrap 시에 사용하던 공급자들을 app 모듈 스코프로 변경
    - 수동으로 inject 해줘야했던것들을 nest DI에 맡김
    
    refactor-#21

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module, ValidationPipe } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModuleOptions } from './global/configs/config-module-options';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './auth/users/users.module';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { HttpExceptionFilter } from './global/filters/http-exception.filter';
 import { ValidationExceptionFilter } from './global/filters/validation-exception.filter';
 import { CategoriesModule } from './categories/categories.module';
 import { BudgetsModule } from './budgets/budgets.module';
 import { ExpendsModule } from './expends/expends.module';
+import { TransformInterceptor } from './global/interceptors/transform.interceptor';
+import { validationExceptionFactory } from './global/factories/validation-exception.factory';
 
 @Module({
 	imports: [
@@ -29,6 +31,22 @@ import { ExpendsModule } from './expends/expends.module';
 		{
 			provide: APP_FILTER,
 			useClass: ValidationExceptionFilter,
+		},
+		{
+			provide: APP_PIPE,
+			useValue: new ValidationPipe({
+				transform: true,
+				whitelist: true,
+				exceptionFactory: validationExceptionFactory,
+			}),
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: TransformInterceptor,
+		},
+		{
+			provide: APP_INTERCEPTOR,
+			useClass: ClassSerializerInterceptor,
 		},
 	],
 })
